27481b492e7e8f2826c0d0621be1a846c98dfbed
Towhidul islam shakil <hshakil320@gmail.com>
2021-01-22 22:58:55 +0600

    add comment

--- assignment.js
+++ assignment.js
@@ -1,36 +1,54 @@
 // kilometerToMeter...
+// using simple formula to find meter to kilometer..
+
 function kilometerToMeter(kilometer){
 
-     const meter = (kilometer * 1000);
-     return meter;
+    if (kilometer > 0)
+    {
+      const meter = (kilometer * 1000);
+      return meter;
+    }
+    else {
+      console.log("Please give the right input") ;
+    }
 } 
-console.log(kilometerToMeter(7));      //Kilometer is "7000"
-
+console.log(kilometerToMeter(6));
 
 
 
 // bugetCalculator... 
+// Caculate the totall cost of different iteam
+
 function bugetCalculator(watch,mobile,laptop){
+
+  if (watch,mobile,laptop > 0){
 
     var iteamOne = (watch * 50);
     var iteamTwo = (mobile * 100);
-    var iteamThree = (laptop * 500);
+    var iteamThree = (laptop * 500);   
 
     var totallCost = iteamOne + iteamTwo + iteamThree;
 
     return totallCost; 
+
+  }
+    else{
+      console.log("Please give the right input") ;
+    }
 }
 
-console.log(bugetCalculator(4,7,3));  // Buget Calculate is "2400"
-
+console.log(bugetCalculator(1,4,7)); 
 
 
 
 // hotelCost...
+ //using conditional (if,else if,else) to find hotelCost with two times discount..
 function hotelCost(day){
+
+  if (day > 0){
     if(day<=10){
        var totallCost = day*100;
-    }
+    }                                  
     else if(day<=20){
        var  first = 10*100;
        var  firstDiscount = (day-10)*80;
@@ -42,21 +60,24 @@ function hotelCost(day){
        var secondDiscount  = (day-20)*50;
        var totallCost = first + firstDiscount + secondDiscount;
     }
-      return totallCost;   // Totall cost is "3050"
-
+      return totallCost; 
+  }
+  else{
+    console.log("Please give the right input") ;
+  }
 }
 
-console.log(hotelCost(45));
+console.log(hotelCost(36));
 
 
 
-
-// megaFriend
+// megaFriend...
+//using for loop and length method to find big-string of an array..
 function megaFriend(array) {
  var word = 0;
  var longest;
  
- for (var i = 0; i < array.length; i++) {
+ for (var i = 0; i < array.length; i++){    
    if (array[i].length > word) {
      word = array[i].length;
      longest = array[i];
@@ -66,5 +87,5 @@ function megaFriend(array) {
  }
  
  var friend = megaFriend(["rishab","abuBakar","simi", "faiza", "jax"]);
- console.log(friend);  // Mega Friend is "abuBakar"
+ console.log(friend); 
  
