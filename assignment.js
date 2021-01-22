2f305e1ef58b00bf384937438adb83b4aaab8ea4
Towhidul islam shakil <hshakil320@gmail.com>
2021-01-22 23:04:50 +0600

    changed

--- assignment.js
+++ assignment.js
@@ -1,5 +1,5 @@
 // kilometerToMeter...
-// using simple formula to find meter to kilometer..
+// using simple formula to find meter to kilometer...
 function kilometerToMeter(kilometer){
     if (kilometer > 0)
     {
@@ -15,7 +15,7 @@ console.log(kilometerToMeter(6));
 
 
 // bugetCalculator... 
-// Caculate the totall cost of different iteam
+// Caculate the totall cost of different iteam...
 function bugetCalculator(watch,mobile,laptop){
   if (watch,mobile,laptop > 0){
       var iteamOne = (watch * 50);
@@ -33,7 +33,7 @@ console.log(bugetCalculator(1,4,7));
 
 
 // hotelCost...
- //using conditional (if,else if,else) to find hotelCost with two times discount..
+//using conditional (if,else if,else) to find hotelCost with two times discount...
 function hotelCost(day){
   if (day > 0){
     if(day<=10){
@@ -61,7 +61,7 @@ console.log(hotelCost(36));
 
 
 // megaFriend...
-//using for loop and length method to find big-string of an array..
+//using for loop and length method to find big-string of an array...
 function megaFriend(array) {
  var word = 0;
  var longest;
